41b03b5e7373ca44799d7985158fe7e2a5e352c7
ialnezami <ibrahim.alnezami@gmail.com>
2023-04-29 12:18:00 +0200

    chore: add index.html

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import * as cors from 'cors';
 import * as express from 'express';
+import * as path from 'path';
 async function bootstrap() {
   const expressApp = express();
   const app = await NestFactory.create(
     AppModule,
     new ExpressAdapter(expressApp),
   );
+  app.use(express.static(path.join(__dirname, '..', 'public')));
   app.use(cors());
   const port = process.env.PORT || 3000;
   await app.listen(port);
